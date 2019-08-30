2f27ac2452a48abfae81d2d61fd8aaf0d0694ae8
sporter399 <sjporter399@gmail.com>
2019-08-29 19:31:26 -0600

    finish FlickList 0 Studio

---
+++ flicklist.js
@@ -0,0 +1,23 @@
+
+
+var api = {
+	root: "https://api.themoviedb.org/3",
+	token: "1f8818270d9286eff7c73f743eab9da7"
+}
+
+/**
+ * Makes an AJAX request to themoviedb.org, asking for some movies
+ * if successful, prints the results to the console
+ */
+function testTheAPI() {
+	fetch(`${api.root}/discover/movie?api_key=${api.token}`)
+		.then(resp => resp.ok ? resp : Promise.reject(resp))
+		.then(function(response) {
+			console.log("We got a response from The Movie DB!");
+			console.log(response);
+		})
+}
+
+
+console.log("The script loaded!");
+testTheAPI();
