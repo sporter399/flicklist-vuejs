30f015031959aa80e0ba5b6d49a31e0cc5ea786b
sporter399 <sjporter399@gmail.com>
2019-09-02 09:56:53 -0600

    another commit of studio0

--- flicklist.js
+++ flicklist.js
@@ -10,12 +10,15 @@ var api = {
  * if successful, prints the results to the console
  */
 function testTheAPI() {
-	fetch(`${api.root}/discover/movie?api_key=${api.token}`)
+	fetch(`${api.root}/configuration?api_key=${api.token}`)
 		.then(resp => resp.ok ? resp : Promise.reject(resp))
 		.then(function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-		})
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+        }).catch(function(err) {
+            console.log(err);
+        });
 }
 
 
